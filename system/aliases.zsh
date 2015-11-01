d68e4c22ef2eeb57a1b383f04c5d103d8e5d01d1
Thomas Maroulis <thomasmaroulis@gmail.com>
2015-11-01 22:59:51 +0000

    Added ls aliases if gls is not present

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -7,6 +7,11 @@ then
   alias l="gls -lAh --color"
   alias ll="gls -l --color"
   alias la='gls -A --color'
+else
+  alias ls="ls -F --color"
+  alias l="ls -lAh --color"
+  alias ll="ls -l --color"
+  alias la="ls -A --color"
 fi
 
 alias less="less -SI -R"
